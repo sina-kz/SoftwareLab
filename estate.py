073d3ac7e03649c26253af6ea502e7f65a1ec4be
Sepehr Feli <sepehr.feli.1377@gmail.com>
2021-10-17 15:38:33 +0330

    Apartment, House and Store subclasses created

--- estate.py
+++ estate.py
@@ -14,3 +14,29 @@ class EstateAbstract(ABC):
     @abstractmethod
     def show_description(self):
         pass
+
+
+class Apartment(EstateAbstract):
+    def __init__(self, has_elevator, has_parking, floor, *args, **kwargs):
+        self.has_elevator = has_elevator
+        self.has_parking = has_parking
+        self.floor = floor
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f"Apartment {self.id}\t area: {self.area}")
+
+
+class House(EstateAbstract):
+    def __init__(self, has_yard, floors_count, *args, **kwargs):
+        self.has_yard = has_yard
+        self.floors_count = floors_count
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f"House: {self.id}")
+
+
+class Store(EstateAbstract):
+    def show_description(self):
+        print(f"Store: {self.id}")
